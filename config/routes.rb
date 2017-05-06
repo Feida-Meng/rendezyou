50bed07beaf68dd4d4c1219931fbc28a39be5e7f
EDWIN AU <edwinau@rogers.com>
2017-05-06 16:43:55 -0400

    generated User Controller with NEW EDIT and SHOW

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/show'
+
+  get 'users/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
