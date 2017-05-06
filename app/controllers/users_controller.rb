d4f5e6842ced6a27418f660b8c72f4bf08a794f5
EDWIN AU <edwinau@rogers.com>
2017-05-06 16:53:58 -0400

    user controllder added UPDATE and DESTROY methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,29 @@
 class UsersController < ApplicationController
   def new
-  end
-
-  def show
+    @user = User.new
   end
 
   def edit
+    @user = current_user
   end
+
+  def show
+    @user = current_user
+    @tours = Tour.all
+  end
+
+  def update
+    @user = current_user
+      if @user.update_attributes(user_params)
+        redirect_to tours_url
+      else
+        render :edit
+      end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :username, :email, :phone, :password, :password_confirmation, :picture)
+  end
+  
 end
