1fbd1c13047cc8b77ea0eea7dcb17222998c6561
EDWIN AU <edwinau@rogers.com>
2017-05-06 17:08:00 -0400

    sessions controller added CREATE and DESTROY methods.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user =
+    User.find_by_username(params[:username])
+      if user && user.authenticate(params[:password])
+          session[:user_id] = user.id
+          redirect_to tours_url, :notice =>
+          "You are now logged in."
+      else
+        flash.now[:alert] = "Invalid username or password"
+        render "new"
+      end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to tours_url, notice: "You are now logged out."
   end
 end
