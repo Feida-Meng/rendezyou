ffdc95d398e99428ce2145c7532a26934a43ef5e
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-05-07 12:57:51 -0400

    adding routes for controllers, with bookings nested within it

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # resources :tours do
+  resources :tours do
     resources :bookings, except: %i(index )
-  # end
+  end
 
 
 end
