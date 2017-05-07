2887e5b26d02ebffa67718d838290c3bd861035b
codonculous <feidameng01@gmail.com>
2017-05-07 10:36:54 -0400

    create tour_session method in schedule

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,5 +1,9 @@
 class Schedule < ApplicationRecord
   has_many :bookings
   belongs_to :tour
-  
+
+  def tour_session
+    "From #{tour_start_time.strftime("%Y %b %d, %H : %M")} to #{tour_end_time.strftime("%Y %b %d, %H : %M")}"
+  end
+
 end
