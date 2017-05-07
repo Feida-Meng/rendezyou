18d79e9a34d29f50ed2579dd64e3e0abce991ea4
codonculous <feidameng01@gmail.com>
2017-05-07 07:49:39 -0400

    rename tourtime to tour_start_time

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170505190355) do
+ActiveRecord::Schema.define(version: 20170507114518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,10 +21,11 @@ ActiveRecord::Schema.define(version: 20170505190355) do
     t.boolean "confirm"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "tour_id"
   end
 
   create_table "schedules", force: :cascade do |t|
-    t.datetime "tourtime"
+    t.datetime "tour_start_time"
     t.integer "tour_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
