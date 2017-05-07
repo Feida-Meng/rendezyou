92fbca337540078887f34283fd83025d9f07f8fe
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-05-07 14:42:20 -0400

    clearing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-user = User.create({name: "test",
-                    password: "password"
-                    })
