119e8c2d145c78bca0d86dce7882001a11ff3b15
codonculous <feidameng01@gmail.com>
2017-05-07 08:31:32 -0400

    add create action, load_tour, load_user and booking params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,36 @@
 class BookingsController < ApplicationController
+  before_action :load_tour,except: %i(load_user booking_params)
+  # before_action :load_tour,except: %i(load_tour booking_params)
+
   def new
     @booking = Booking.new
   end
+
+  def create
+
+    @booking = @tour.bookings.build(booking_params)
+
+    if @booking.booking(@tour)
+      redirect_to tour_path(@tour)
+    else
+      render :new
+    end
+
+  end
+
+private
+ def load_tour
+   @tour = Tour.find(params[:tour_id]);
+ end
+
+ def load_user
+   @user = User.find(params[:user_id]);
+ end
+
+  def booking_params
+  # params[:booking][:user_id] = 1
+  params.require(:booking).permit( :user_id, :schedule_id, :booking_size)
+  end
+
 
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,18 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :tour
 
+  def booking(current_tour)
+    tourtime = current_tour.schedules.find(schedule_id)
+
+    capacity_check = tourtime.max_capacity > tourtime.current_capacity + booking_size
+
+    if capacity_check && self.save
+      tourtime.current_capacity += booking_size
+    else
+      false
+    end
+
+  end
 
 
 end
