e28df9900fb4d9e44ef3904bc44acb48d644b8bf
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-05-09 22:14:47 -0400

    adding validations to schedule to ensure times are not in the past

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,9 +3,27 @@ class Schedule < ApplicationRecord
   belongs_to :tour
 
   validates :max_capacity, presence: true
+  validate :validate_dates
 
   def tour_session
-    "From #{tour_start_time.strftime("%Y %b %d, %H : %M")} to #{tour_end_time.strftime("%Y %b %d, %H : %M")}"
+    "#{tour_start_time.strftime("%b %d, %Y at %H:%M")} to #{tour_end_time.strftime("%H:%M")}"
   end
+
+  def date_in_past(time)
+   if time < Time.now
+     return true
+   end
+  end
+
+  def validate_dates
+    if date_in_past(tour_start_time)
+      errors.add(:tour_start_time, "cannot be in the past")
+    elsif date_in_past(tour_end_time)
+      errors.add(:tour_end_time, "cannot be in the past")
+    elsif tour_end_time <= tour_start_time
+      errors.add(:tour_end_time, "cannot be before the tour starts")
+    end
+  end
+
 
 end
