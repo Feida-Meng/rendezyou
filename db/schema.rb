7ae8414dc44553882990fe5b2812661fb5b2477b
codonculous <feidameng01@gmail.com>
2017-05-11 17:19:24 -0400

    create model and table for country and city

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170510021535) do
+ActiveRecord::Schema.define(version: 20170511211348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,20 @@ ActiveRecord::Schema.define(version: 20170510021535) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "booking_size"
+  end
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.bigint "country_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["country_id"], name: "index_cities_on_country_id"
+  end
+
+  create_table "countries", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "schedules", force: :cascade do |t|
@@ -58,4 +72,5 @@ ActiveRecord::Schema.define(version: 20170510021535) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "cities", "countries"
 end
