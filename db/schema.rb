3594f0e46e92ca5e62f813cceb1e215be703f3a8
codonculous <feidameng01@gmail.com>
2017-05-11 21:50:24 -0400

    rename address to rendezvous point from table tours

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170512014416) do
+ActiveRecord::Schema.define(version: 20170512014832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 20170512014416) do
     t.string "name"
     t.string "description"
     t.string "city"
-    t.string "address"
+    t.string "rendezvous_point"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
