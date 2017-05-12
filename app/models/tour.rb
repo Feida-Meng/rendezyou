db61abbcba05950acfb46a9be3acf9b861d3c350
codonculous <feidameng01@gmail.com>
2017-05-11 21:39:08 -0400

    change the category data type from string to integer, and enum category

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -3,8 +3,8 @@ class Tour < ApplicationRecord
   has_many :bookings, through: :schedules
   has_many :schedules
 
-
-  COUNTRIES_AND_CITIES = {Canada: ['Toronto','Vancouver','Calgory','Edmonton','Ottawa'], China:['Beijing','Shanghai','Zhengzhou','HK','Zhengzhou'], Philippines:['Manila','Davao','Quezon','Caloocan','Cebu'], EU:['Pairs','Berlin','London','Moscow','Rome']}
+  enum category: [ :nature, :arts, :historical ]
+  # COUNTRIES_AND_CITIES = {Canada: ['Toronto','Vancouver','Calgory','Edmonton','Ottawa'], China:['Beijing','Shanghai','Zhengzhou','HK','Zhengzhou'], Philippines:['Manila','Davao','Quezon','Caloocan','Cebu'], EU:['Pairs','Berlin','London','Moscow','Rome']}
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511212143) do
+ActiveRecord::Schema.define(version: 20170512013359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,9 +66,9 @@ ActiveRecord::Schema.define(version: 20170511212143) do
     t.string "country"
     t.string "address"
     t.integer "user_id"
-    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category"
   end
 
   create_table "users", force: :cascade do |t|
