3edd2388c525d7112641cdeb81410020f1570748
EDWIN AU <edwinau@rogers.com>
2017-05-11 20:31:38 -0400

    Search Bar works

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -6,5 +6,8 @@ class Tour < ApplicationRecord
 
   COUNTRIES_AND_CITIES = {Canada: ['Toronto','Vancouver','Calgory','Edmonton','Ottawa'], China:['Beijing','Shanghai','Zhengzhou','HK','Zhengzhou'], Philippines:['Manila','Davao','Quezon','Caloocan','Cebu'], EU:['Pairs','Berlin','London','Moscow','Rome']}
 
+  def self.search(search)
+    where("name LIKE ? OR description LIKE ? OR category LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%") 
+  end
 
 end
