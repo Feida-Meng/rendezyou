8d9a6109c1bfc8977cf5691f42fb8939c7859566
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-05-15 12:52:33 -0400

    creating booking and welcome email in user_mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,9 +2,5 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
 
-  def booking_confirmation_email(user)
-    @user = user
-    mail(to: @user.email,
-         subject: "Booking Confirmation")
-  end
+
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,16 @@
 class UserMailer < ApplicationMailer
+
+  default from: 'notifications@example.com'
+
+  def booking_confirmation_email(user)
+    @user = user
+    mail(to: @user.email,
+         subject: "Booking Confirmation")
+  end
+
+  def welcome_email(user)
+    @user = user
+    mail(to: user.email, subject: "Welcome to Rendezyou!")
+  end
+  
 end
