8cf5482053a9a11295e1fafd777efb1c734dafcb
EDWIN AU <edwinau@rogers.com>
2017-05-15 11:34:55 -0400

    removed window.height console log in Layout JS

--- app/assets/javascripts/layouts.js
+++ app/assets/javascripts/layouts.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
   $('.nav_bar').css('background',"transparent");
   $(window).scroll(function(){
-    console.log($(window.height))
     if ($(this).scrollTop()>150){
         $('.nav_bar').css('background', 'rgba(0,0,0,.8)').css('transition','0.3s ease-in-out');
         // $('a').css('font-size', '98%').css('transition','0.3s ease-in-out');
