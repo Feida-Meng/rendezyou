06ad28a6578315744ea8086118a48da9ef0c968b
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-05-15 12:51:31 -0400

    adding a welcome email method to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
+      UserMailer.welcome_email(@user).deliver
       redirect_to profile_path
     else
       render :new
