c79311fc68fa3130b9399bc4c10151a1f066c619
codonculous <feidameng01@gmail.com>
2017-05-16 15:04:06 -0400

    pull from github

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,49 @@
+class ReviewsController < ApplicationController
+
+  before_action :load_tour,except: %i(load_schedule review_params)
+  before_action :ensure_logged_in
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new
+    @review = @tour.reviews.build(review_params)
+    @review.author_id = current_user.id
+    if @review.save
+      redirect_to tour_path(@tour)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+    ensure_review_author
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    @review.update_attributes(review_params)
+    if @review.save
+      redirect_to tour_path(@tour)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
+  private
+   def load_tour
+     @tour = Tour.find(params[:tour_id])
+   end
+
+  def review_params
+    params.require(:review).permit(:rating, :comment, :tour_id, :created_at)
+  end
+
+
+end
