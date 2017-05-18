3aed26a5400199c0199e5577d09dc0ab09cc8d07
EDWIN AU <edwinau@rogers.com>
2017-05-18 11:15:52 -0400

    User_show page now links

--- app/assets/javascripts/layouts.js
+++ app/assets/javascripts/layouts.js
@@ -1,20 +1,20 @@
 
 $(document).ready(function(){
-  $('.nav_bar').css('background',"transparent");
-  $(window).scroll(function(){
-
-    if ($(this).scrollTop()>150){
-        $('.nav_bar').css('background', 'rgba(0,0,0,.8)').css('transition','0.3s ease-in-out');
-        // $('a').css('font-size', '98%').css('transition','0.3s ease-in-out');
-      }else if ($(this).scrollTop()>90){
-        $('.nav_bar').css('background', 'rgba(0,0,0,0.5)').css('transition','0.3s ease-in-out');
-      }else if ($(this).scrollTop()>40){
-        $('.nav_bar').css('background', 'rgba(0,0,0,0.35)').css('transition','0.3s ease-in-out');
-      }else{
-        $('.nav_bar').css('background', 'rgba(0,0,0,0)').css('transition','0.3s ease-in-out');
-        // $('a').css('font-size', '100%').css('transition','0.3s ease-in-out');
-      }
-  });
+  // $('.nav_bar').css('background',"transparent");
+  // $(window).scroll(function(){
+  //
+  //   if ($(this).scrollTop()>150){
+  //       $('.nav_bar').css('background', 'rgba(0,0,0,.8)').css('transition','0.3s ease-in-out');
+  //       // $('a').css('font-size', '98%').css('transition','0.3s ease-in-out');
+  //     }else if ($(this).scrollTop()>90){
+  //       $('.nav_bar').css('background', 'rgba(0,0,0,0.5)').css('transition','0.3s ease-in-out');
+  //     }else if ($(this).scrollTop()>40){
+  //       $('.nav_bar').css('background', 'rgba(0,0,0,0.35)').css('transition','0.3s ease-in-out');
+  //     }else{
+  //       $('.nav_bar').css('background', 'rgba(0,0,0,0)').css('transition','0.3s ease-in-out');
+  //       // $('a').css('font-size', '100%').css('transition','0.3s ease-in-out');
+  //     }
+  // });
 
   //login modal
   $('.a_login').on('click', function(e){
