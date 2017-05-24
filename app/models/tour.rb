5b40a6d515694eecbdf448814a4e89242d65f4b6
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-05-24 13:19:08 -0400

    booking form validation - booking size must exist

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -8,6 +8,7 @@ class Tour < ApplicationRecord
   has_attached_file :tourpic, styles: { medium: "300x300>", thumb: "100x100>", big: "400x400>"}, default_url: "https://s3.ca-central-1.amazonaws.com/rendezyou-heroku/default imgs/top.jpg"
 
   validates :duration_in_ms, numericality: { greater_than: 0 }
+  validates :name, :description, :rendezvous_point, :capacity, presence: true
   validates_attachment_content_type :tourpic, content_type: /\Aimage\/.*\z/
 
   # converting duration input to ms before saving
